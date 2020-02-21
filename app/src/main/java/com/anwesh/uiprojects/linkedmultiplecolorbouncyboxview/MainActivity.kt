568423fd400d589519ba8fb09def966b1b9560aa
Anwesh43 <anweshthecool0@gmail.com>
2020-02-21 14:48:49 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmultiplecolorbouncyboxview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmultiplecolorbouncyboxview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedmultiplecolorbouncyboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
