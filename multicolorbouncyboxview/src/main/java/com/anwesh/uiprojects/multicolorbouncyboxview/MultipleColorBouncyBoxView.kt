e184f07f884697b818973e7bd1a9e43fa50950b8
Anwesh43 <anweshthecool0@gmail.com>
2020-02-21 14:04:05 +0530

    adding MultipleColorBouncyBoxView class

--- multicolorbouncyboxview/src/main/java/com/anwesh/uiprojects/multicolorbouncyboxview/MultipleColorBouncyBoxView.kt
+++ multicolorbouncyboxview/src/main/java/com/anwesh/uiprojects/multicolorbouncyboxview/MultipleColorBouncyBoxView.kt
@@ -55,3 +55,21 @@ fun Canvas.drawMCBBNode(i : Int, scale : Float, paint : Paint) {
     drawMultiColorBouncyBox(scale, size, paint)
     restore()
 }
+
+class MultipleColorBouncyBoxView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
