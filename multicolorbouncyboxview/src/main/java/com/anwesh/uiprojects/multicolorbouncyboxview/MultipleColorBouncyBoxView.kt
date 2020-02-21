f3c3acc8b4b7a20bb57a1b76d36cddd2b7c25347
Anwesh43 <anweshthecool0@gmail.com>
2020-02-21 13:59:47 +0530

    adding Canvas extension methods

--- multicolorbouncyboxview/src/main/java/com/anwesh/uiprojects/multicolorbouncyboxview/MultipleColorBouncyBoxView.kt
+++ multicolorbouncyboxview/src/main/java/com/anwesh/uiprojects/multicolorbouncyboxview/MultipleColorBouncyBoxView.kt
@@ -18,8 +18,40 @@ val scGap : Float = 0.02f
 val strokeFactor : Int = 90
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawColorBouncyBox(i : Int, scale : Float, size : Float, paint : Paint) {
+    val gap : Float = size / rects
+    val sf : Float = scale.sinify().divideScale(i, rects)
+    val wSize : Float = gap * i + gap * sf
+    save()
+    rotate(deg * sf)
+    drawRect(RectF(-wSize / 2, -wSize / 2, wSize / 2, wSize / 2), paint)
+    restore()
+}
+
+fun Canvas.drawMultiColorBouncyBox(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..(rects - 1)) {
+        drawColorBouncyBox(j, scale, size, paint)
+    }
+}
+
+fun Canvas.drawMCBBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h)
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+
+    save()
+    translate(w / 2, h / 2)
+    drawMultiColorBouncyBox(scale, size, paint)
+    restore()
+}
